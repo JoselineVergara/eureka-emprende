ca839f667f0665b87925de9e3e7ed315c9885c28
Josie <joselinevergara@uees.edu.ec>
2025-10-10 23:25:24 -0500

    Tipo Emprendimiento Crud

---
+++ src/main/java/com/example/eureka/general/dto/TipoEmprendimientoDTO.java
@@ -0,0 +1,11 @@
+package com.example.eureka.general.dto;
+
+import lombok.Data;
+
+@Data
+public class TipoEmprendimientoDTO {
+
+    private Integer id;
+    private String tipo;
+    private String subTipo;
+}
